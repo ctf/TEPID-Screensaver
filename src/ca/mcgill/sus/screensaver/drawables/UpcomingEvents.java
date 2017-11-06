e7a818f07d8781fa765fdee8ee581f660fa72b62
Seiji Amasawa <samasawa95@gmail.com>
2017-11-06 15:06:04 -0500

    change ’ to \u2019 to avoid encoding issue on windoze

--- src/ca/mcgill/sus/screensaver/drawables/UpcomingEvents.java
+++ src/ca/mcgill/sus/screensaver/drawables/UpcomingEvents.java
@@ -21,29 +21,29 @@ import ca.mcgill.sus.screensaver.FontManager;
 import ca.mcgill.sus.screensaver.Stage;
 
 public class UpcomingEvents implements Drawable {
-	
+
 	private final Queue<String> entries = DataFetch.getInstance().upcomingEvents;
 	private ScheduledFuture<?> marqueeHandle;
 	public final int y;
 	private int alphaEntry = 0;
 	private String currentEntry = "";
-	private final static String[] titles = {"upcoming", "stay in the know", "don’t be lazy", "good CTFers are informed CTFers"};
-	private final int color, maxAlpha;	//maximum alpha value for the marquee during fadein 
+	private final static String[] titles = {"upcoming", "stay in the know", "don\u2019t be lazy", "good CTFers are informed CTFers"};
+	private final int color, maxAlpha;	//maximum alpha value for the marquee during fadein
 	private Stage parent;
-	
+
 
 	/**Constructor
 	 * @param y		//the y position
 	 * @param color	//the colour of the text
 	 */
-	public UpcomingEvents(int y, int color) 
+	public UpcomingEvents(int y, int color)
 	{
 		this.y = y;		//y position
 		//handles the colour. calibrates the maximum alpha to not excede the alpha specified
 		this.color = 0xffffff & color; //the text colour
-		if (((color >> 24) & 0xff) > 0) 
-			{maxAlpha = (color >> 24) & 0xff;} 
-		else 
+		if (((color >> 24) & 0xff) > 0)
+			{maxAlpha = (color >> 24) & 0xff;}
+		else
 			{maxAlpha = 0xff;}
 		if (marqueeHandle == null && !entries.isEmpty()) startMarquee();
 		DataFetch.getInstance().addChangeListener(new Runnable() {
@@ -52,13 +52,13 @@ public class UpcomingEvents implements Drawable {
 			}
 		});
 	}
-	
+
 	/* (non-Javadoc)
-	 * 
+	 *
 	 * @see ca.mcgill.sus.screensaver.Drawable#draw(java.awt.Graphics2D, int, int)
 	 */
 	@Override
-	public void draw(Graphics2D g, int canvasWidth, int canvasHeight) 
+	public void draw(Graphics2D g, int canvasWidth, int canvasHeight)
 	{
 		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 		//draws the "CURRENT VOLUNTEERS ON DUTY"
@@ -69,7 +69,7 @@ public class UpcomingEvents implements Drawable {
 		//draws the person logged in
 		g.setFont(FontManager.getInstance().getFont("nhg-thin.ttf").deriveFont(24f));
 		g.setColor(new Color((alphaEntry << 24) | color, true));
-		synchronized(currentEntry) 
+		synchronized(currentEntry)
 		{
 			g.setColor(new Color((alphaEntry << 24) | color, true));	//sets the colour for the font
 			g.drawString(currentEntry, canvasWidth / 2 - g.getFontMetrics().stringWidth(currentEntry) / 2, y + 40); //draws the font
@@ -77,14 +77,14 @@ public class UpcomingEvents implements Drawable {
 	}
 	/**Changes from one entry to another, with a fadeout/fadein effect
 	 * @param entry	The thing to fade
-	 * 
+	 *
 	 */
-	public void changeEntry(final String entry) 
+	public void changeEntry(final String entry)
 	{
-		new Thread("Change Entry") 
+		new Thread("Change Entry")
 		{
 			@Override
-			public void run() 
+			public void run()
 			{
 				final int fadeInMs = 1400, fadeOutMs = 800;
 				while (alphaEntry > 0)
@@ -93,11 +93,11 @@ public class UpcomingEvents implements Drawable {
 					if (parent != null) parent.safeRepaint();
 					UpcomingEvents.sleep(fadeInMs / maxAlpha);
 				}
-				synchronized(UpcomingEvents.this.currentEntry) 
+				synchronized(UpcomingEvents.this.currentEntry)
 				{
 					UpcomingEvents.this.currentEntry = entry;
 				}
-				while (alphaEntry < maxAlpha) 
+				while (alphaEntry < maxAlpha)
 				{
 					alphaEntry++;
 					if (parent != null) parent.safeRepaint();
@@ -106,8 +106,8 @@ public class UpcomingEvents implements Drawable {
 			}
 		}.start();
 	}
-	
-	/**A function which causes the thread to wait for a time 
+
+	/**A function which causes the thread to wait for a time
 	 * @param ms	The time for which the thread should do nothing
 	 */
 	public static void sleep(long ms) {
@@ -117,16 +117,16 @@ public class UpcomingEvents implements Drawable {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**Starts the Marquee and iterates through all of the items
-	 * 
+	 *
 	 */
 	public void startMarquee() {
 		final Runnable marquee = new Runnable() {
 			Iterator<String> iterNames = entries.iterator();
 			public void run() {
 				try {
-					if (!iterNames.hasNext()) 
+					if (!iterNames.hasNext())
 						{iterNames = entries.iterator();}
 					if (iterNames.hasNext())
 						{changeEntry(iterNames.next());}
@@ -139,11 +139,11 @@ public class UpcomingEvents implements Drawable {
 		if (marqueeHandle != null) marqueeHandle.cancel(false);
 		marqueeHandle = Executors.newScheduledThreadPool(1).scheduleAtFixedRate(marquee, 0, 5, TimeUnit.SECONDS);
 	}
-	
+
 	public void stopMarquee() {
 		if (marqueeHandle != null) marqueeHandle.cancel(false);
 	}
-	
+
 	public static String getTitle() {
 		MessageDigest md5;
 		try {
@@ -155,7 +155,7 @@ public class UpcomingEvents implements Drawable {
 		double ind = (double) Integer.parseInt(hash.substring(0,2) + hash.substring(hash.length()-2), 16) / 0xffff;
 		return titles[(int) Math.floor(ind * titles.length)].toUpperCase();
 	}
-	
+
 	@Override
 	public void setParent(Stage parent) {
 		this.parent = parent;
