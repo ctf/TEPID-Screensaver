89ab96706e2a6164ec548e7def3dbbfa9c52fa26
Seiji Amasawa <samasawa95@gmail.com>
2017-12-15 18:14:05 -0500

    change name of wix output msi to match previous versions

--- windows/wix.bat
+++ windows/wix.bat
@@ -2,5 +2,5 @@ set PRODUCTVERSION=3.2.0
 "%WIX%"\bin\heat dir ctfscreen-data -ag -dr dir_ctfscreen_data -suid -cg libDir -out libDir.wxs
 "%WIX%"\bin\candle -arch x64 libDir.wxs
 "%WIX%"\bin\candle installer.wxs
-"%WIX%"\bin\light installer.wixobj libDir.wixobj -b ctfscreen-data -out ctfscreen-%PRODUCTVERSION%.msi
+"%WIX%"\bin\light installer.wixobj libDir.wixobj -b ctfscreen-data -out ctfscreensaver-%PRODUCTVERSION%.msi
 del installer.wixobj libDir.wixObj ctfscreen-%PRODUCTVERSION%.wixpdb
