a7bdc9caff9d4143c9da38adc968e3c860cd37a1
Daniel Goldman <merkavabuilder@gmail.com>
2018-05-26 19:08:02 -0400

    Calendar and gravatar search are pulled from configs

--- src/main/java/ca/mcgill/sus/screensaver/Config.kt
+++ src/main/java/ca/mcgill/sus/screensaver/Config.kt
@@ -24,6 +24,8 @@ object Config : WithLogging() {
     val report_malfunctioning_to: String
     val announcement_slide_directory: String
     val background_picture_directory: String
+    val GOOGLE_CUSTOM_SEARCH_KEY: String
+    val ICS_CALENDAR_ADDRESS: String
 
     val DEBUG: Boolean
 
@@ -44,6 +46,8 @@ object Config : WithLogging() {
         report_malfunctioning_to = PropsScreensaver.REPORT_MALFUNCTIONING_COMPUTER_TEXT
         announcement_slide_directory = PropsScreensaver.ANNOUNCEMENT_SLIDE_LOCATION
         background_picture_directory = PropsScreensaver.BACKGROUND_PICTURE_LOCATION
+        GOOGLE_CUSTOM_SEARCH_KEY = PropsScreensaver.GOOGLE_CUSTOM_SEARCH_KEY
+        ICS_CALENDAR_ADDRESS = PropsScreensaver.ICS_CALENDAR_ADDRESS
 
         if (DEBUG) LogUtils.setLoggingLevel(log, Level.TRACE)
 
