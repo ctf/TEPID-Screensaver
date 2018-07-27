f40e3f458b8ac7215d56e5af3fbfe941dc63216d
Daniel Goldman <merkavabuilder@gmail.com>
2018-07-27 02:22:35 -0400

    collapse empty catch blocks

--- src/main/java/ca/mcgill/sus/screensaver/Util.java
+++ src/main/java/ca/mcgill/sus/screensaver/Util.java
@@ -365,8 +365,7 @@ public class Util {
 		try {
 			MessageDigest md = MessageDigest.getInstance("MD5");
 			return hex(md.digest(message.getBytes("CP1252")));
-		} catch (NoSuchAlgorithmException e) {
-		} catch (UnsupportedEncodingException e) {
+		} catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
 		}
 		return null;
 	}
