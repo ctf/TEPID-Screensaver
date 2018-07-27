79a18ba86af8c34f367db740ac524886677b59fd
Daniel Goldman <merkavabuilder@gmail.com>
2018-07-27 02:22:44 -0400

    use stringbuilder

--- src/main/java/ca/mcgill/sus/screensaver/Util.java
+++ src/main/java/ca/mcgill/sus/screensaver/Util.java
@@ -353,7 +353,7 @@ public class Util {
 	}
 	
 	public static String hex(byte[] array) {
-		StringBuffer sb = new StringBuffer();
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < array.length; ++i) {
 			sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(
 					1, 3));
