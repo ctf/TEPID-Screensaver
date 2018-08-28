dd6ed6c48cd218c590cd13f90a5828ccfbfdaee6
Daniel Goldman <merkavabuilder@gmail.com>
2018-08-27 20:40:25 -0400

    Refactor: extracted main display rendering functions

--- src/main/java/ca/mcgill/sus/screensaver/ScreensaverMainDisplay.java
+++ src/main/java/ca/mcgill/sus/screensaver/ScreensaverMainDisplay.java
@@ -24,21 +24,10 @@ public class ScreensaverMainDisplay extends BlurredScreensaverFrame {
 					stage.clear();
 					if (networkUp) {
 						//if network is up use regular set of widgets
-						stage.addDrawable(new JobList(500));
-						stage.addDrawable(new Slideshow(10_000, 1000, 420));
-						stage.addDrawable(new Clock("hh:mm a", Main.TEXT_COLOR & 0x88000000, 65));
-						stage.addDrawable(new InvertingHeader(System.getenv("computerName"), 18, 20, Main.TEXT_COLOR, false).setAlignment(Header.ALIGN_LEFT));
-						stage.addDrawable(new UserInfoBar(48, 420));
-//						stage.addDrawable(new Marquee(350, Main.TEXT_COLOR));
-						if (Main.OFFICE_COMPUTER && Main.LOGGED_IN) stage.addDrawable(new ProfilePic());
+						displayContents();
 					} else {
 						//if network is down display special error screen
-						stage.addDrawable(new Error());
-						stage.addDrawable(new Header("This computer has", 70, 90, 0x000000, false, "constan.ttf"));
-						stage.addDrawable(new Header("No Network Connection", 120, 220, 0x000000, true, "constanb.ttf"));
-						stage.addDrawable(new Header("This is usually the result of a disconnected cable.", 50, 900, 0xff0000, true, "constanb.ttf"));
-						stage.addDrawable(new Header("Please report that this workstation (" + System.getenv("computerName") + ")", 30, 975, 0xff0000, false, "constan.ttf"));
-						stage.addDrawable(new Header("is not functioning to" + Config.INSTANCE.getReport_malfunctioning_to(), 30, 1015, 0xff0000, false, "constan.ttf"));
+						displayError();
 					}
 					stage.setDirty(true);
 				}
@@ -48,5 +37,23 @@ public class ScreensaverMainDisplay extends BlurredScreensaverFrame {
 		DataFetch.getInstance().addChangeListener(checkNetwork);
 	}
 
-	
+	private void displayContents() {
+		stage.addDrawable(new JobList(500));
+		stage.addDrawable(new Slideshow(10_000, 1000, 420));
+		stage.addDrawable(new Clock("hh:mm a", Main.TEXT_COLOR & 0x88000000, 65));
+		stage.addDrawable(new InvertingHeader(System.getenv("computerName"), 18, 20, Main.TEXT_COLOR, false).setAlignment(Header.ALIGN_LEFT));
+		stage.addDrawable(new UserInfoBar(48, 420));
+//						stage.addDrawable(new Marquee(350, Main.TEXT_COLOR));
+		if (Main.OFFICE_COMPUTER && Main.LOGGED_IN) stage.addDrawable(new ProfilePic());
+	}
+
+	private void displayError() {
+		stage.addDrawable(new Error());
+		stage.addDrawable(new Header("This computer has", 70, 90, 0x000000, false, "constan.ttf"));
+		stage.addDrawable(new Header("No Network Connection", 120, 220, 0x000000, true, "constanb.ttf"));
+		stage.addDrawable(new Header("This is usually the result of a disconnected cable.", 50, 900, 0xff0000, true, "constanb.ttf"));
+		stage.addDrawable(new Header("Please report that this workstation (" + System.getenv("computerName") + ")", 30, 975, 0xff0000, false, "constan.ttf"));
+		stage.addDrawable(new Header("is not functioning to" + Config.INSTANCE.getReport_malfunctioning_to(), 30, 1015, 0xff0000, false, "constan.ttf"));
+	}
+
 }
