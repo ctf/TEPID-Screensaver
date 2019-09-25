4d45cf1d90acea2053fc8b17eb2877ff21bcd98c
Daniel <merkavabuilder@gmail.com>
2019-09-25 19:47:41 -0400

    add generic DataFetchable for ITepid

---
+++ src/main/java/ca/mcgill/sus/screensaver/datafetch/ITepidFetch.java
@@ -0,0 +1,35 @@
+package ca.mcgill.sus.screensaver.datafetch;
+
+import ca.mcgill.sus.screensaver.ConfigKt;
+import retrofit2.Call;
+
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+public class ITepidFetch<T> extends DataFetchable<T> {
+
+    interface ApiCall<T> {
+        Call<T> buildCall();
+    }
+
+    private T value;
+
+    private final ApiCall<T> fetchFunction;
+    private final long timeOutInterval;
+
+    ITepidFetch (long _timeOutInterval, ApiCall<T> _fetchFunction){
+        fetchFunction = _fetchFunction;
+        timeOutInterval = _timeOutInterval;
+    }
+
+    @Override
+    public FetchResult<T> fetch() {
+        Future<T> futureVal = ConfigKt.asCompletableFuture(	fetchFunction.buildCall());
+        try {
+            value = futureVal.get(timeOutInterval, TimeUnit.SECONDS);
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        return new FetchResult<T>(value);
+    }
+}
